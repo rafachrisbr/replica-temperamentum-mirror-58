ff5681b3f42571f376e383d7d1c00d3147e5fad8
Rafael Christiano <rafael.christiano@yahoo.com.br>
2025-05-14 14:16:47 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ const Footer: React.FC = () => {
       <div className="max-w-3xl mx-auto">
         <div className="border-t border-[#444444] pt-4"></div>
         <p className="text-[#D4AF37] font-serif uppercase my-3 text-center">DEUS CARITAS EST</p>
-        <p className="text-sm my-2 first-letter:uppercase">este teste é uma ferramenta de autoconhecimento e não substitui o acompanhamento espiritual.</p>
-        <p className="text-xs my-2 first-letter:uppercase">fontes principais: ensinamentos do pe. paulo ricardo (curso "os quatro temperamentos") e canção nova (artigo "qual é o seu temperamento?").</p>
-        <p className="text-sm my-2 text-center first-letter:uppercase">© 2025 rafael christiano</p>
+        <p className="text-sm my-2 first-letter:uppercase">Este teste é uma ferramenta de autoconhecimento e não substitui o acompanhamento espiritual.</p>
+        <p className="text-xs my-2 first-letter:uppercase">Fontes principais: ensinamentos do pe. paulo ricardo (curso "os quatro temperamentos") e canção nova (artigo "qual é o seu temperamento?").</p>
+        <p className="text-sm my-2 text-center first-letter:uppercase">© 2025 Rafael Christiano</p>
       </div>
     </footer>
   );
